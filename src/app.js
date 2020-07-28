b68008d25335d0a967928deacca90e3402293cf3
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-28 18:09:11 +0100

    added input to enter amount

--- src/app.js
+++ src/app.js
@@ -4,27 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
     new Vue({
         el: "#app",
         data: {
-            rates: []
+            rates: {},
+            amount: 0
         },
 
-        computed: {
 
+        computed: {
 
         },
 
         mounted(){
-
+            this.getRates()
         },
 
         methods: {
             getRates: function() {
                 fetch("https://api.exchangeratesapi.io/latest")
                 .then(response => response.json())
-                .then(rates => this.rates = rates)
+                .then(rates => this.rates = rates.rates)
+            }, 
+
+            convertFromEuros: function() {
+                return this.amount * this.rates;
             }
         }
+
     })
-
-
 
 })
